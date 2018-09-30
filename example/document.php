77e2c22dbdeb1c23c128be1e2aee5ba3b0b20ff1
Tiago Chini <tiago.chini@hotmail.com>
2018-09-30 19:45:58 -0400

    Criação do Documents e correção do README.md[documents]

---
+++ example/document.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="style.css">
+
+<div class="form">
+    <form name="env" method="post" enctype="multipart/form-data">
+        <?php
+            require __DIR__ . "/../src/Uploader.php";
+            require __DIR__ . "/../src/Document.php";
+
+            $document = new CoffeeCode\Uploader\Document("uploads", "files");
+
+            if ($_FILES) {
+                $upload = $document->upload($_FILES['file'], $_POST['name']);
+                echo "<p><a href='{$upload}' target='_blank'>Acessar arquivo</a></p>";
+            }
+        ?>
+        <input type="text" name="name" placeholder="Document Name" required/>
+        <input type="file" name="file" required/>
+        <button>Send Document</button>
+    </form>
+</div>
