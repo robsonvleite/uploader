5f5ea5344023240e3e54c14fed1045c4a305d13c
fernando <fherdev@gmail.com>
2020-08-27 03:41:45 -0300

    2 tipos de arquivos adicionados no allowTypes

--- src/File.php
+++ src/File.php
@@ -10,49 +10,51 @@ namespace CoffeeCode\Uploader;
  */
 class File extends Uploader
 {
-    /**
-     * Allow zip, rar, bzip, pdf, doc, docx files
-     * @var array allowed file types
-     * https://www.freeformatter.com/mime-types-list.html
-     */
-    protected static $allowTypes = [
-        "application/zip",
-        'application/x-rar-compressed',
-        'application/x-bzip',
-        "application/pdf",
-        "application/msword",
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    ];
+	/**
+	 * Allow zip, rar, bzip, pdf, doc, docx files
+	 * @var array allowed file types
+	 * https://www.freeformatter.com/mime-types-list.html
+	 */
+	protected static $allowTypes = [
+		"application/zip",
+		"application/octet-stream",
+		"application/x-zip-compressed",
+		'application/x-rar-compressed',
+		'application/x-bzip',
+		"application/pdf",
+		"application/msword",
+		"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+	];
 
-    /**
-     * Allowed extensions to types.
-     * @var array
-     */
-    protected static $extensions = [
-        "zip",
-        "rar",
-        "bz",
-        "pdf",
-        "doc",
-        "docx"
-    ];
+	/**
+	 * Allowed extensions to types.
+	 * @var array
+	 */
+	protected static $extensions = [
+		"zip",
+		"rar",
+		"bz",
+		"pdf",
+		"doc",
+		"docx"
+	];
 
-    /**
-     * @param array $file
-     * @param string $name
-     * @return null|string
-     * @throws \Exception
-     */
-    public function upload(array $file, string $name): string
-    {
-        $this->ext = mb_strtolower(pathinfo($file['name'])['extension']);
+	/**
+	 * @param array $file
+	 * @param string $name
+	 * @return null|string
+	 * @throws \Exception
+	 */
+	public function upload(array $file, string $name): string
+	{
+		$this->ext = mb_strtolower(pathinfo($file['name'])['extension']);
 
-        if (!in_array($file['type'], static::$allowTypes) || !in_array($this->ext, static::$extensions)) {
-            throw new \Exception("Not a valid file type or extension");
-        }
+		if (!in_array($file['type'], static::$allowTypes) || !in_array($this->ext, static::$extensions)) {
+			throw new \Exception("Not a valid file type or extension");
+		}
 
-        $this->name($name);
-        move_uploaded_file($file['tmp_name'], "{$this->path}/{$this->name}");
-        return "{$this->path}/{$this->name}";
-    }
+		$this->name($name);
+		move_uploaded_file($file['tmp_name'], "{$this->path}/{$this->name}");
+		return "{$this->path}/{$this->name}";
+	}
 }
